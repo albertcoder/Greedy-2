fce55b7b89ab6abd8d4720adff0ce7f79cd1ca48
albertcoder <eralbert9191@gmail.com>
2020-07-04 15:14:34 -0700

    task scheduler

---
+++ taskScheduler.java
@@ -0,0 +1,25 @@
+//time complexity 
+//
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if(tasks == null || tasks.length == 0) return 0;
+        int maxFreq = 0;//max times a character appears
+       	int countMaxFreq = 0;//how many ele have the maxFreq
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < tasks.length; i++){//calc maxFreq
+            char c = tasks[i];
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            int count = map.get(c);
+            maxFreq = Math.max(count, maxFreq);
+        }
+        for(char c: map.keySet()){//calc countMaxFreq
+            if(map.get(c) == maxFreq) countMaxFreq++;
+        }
+        int partitions = maxFreq - 1;
+        int empty = (n - countMaxFreq + 1) * partitions;
+        int pending = tasks.length - (maxFreq * countMaxFreq);
+        int idle = Math.max(0, empty - pending);
+        return tasks.length + idle;
+    }
+}
